7a49473838902ee21e1c5ad048fd8de6434dd5f7
Mike Navales <johnmichael.navales@gmail.com>
2025-04-05 06:31:49 +0800

    feat: add sitemetadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,21 @@
-import type { GatsbyConfig, PluginRef } from "gatsby"
-import "dotenv/config"
+import type { GatsbyConfig, PluginRef } from "gatsby";
+import "dotenv/config";
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 const config: GatsbyConfig = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Automeyt`,
+    siteTitleAlt: `AI / Automation`,
+    siteHeadline: `Automeyt - Your AI / Automation Business Partner`,
+    siteUrl: `https://automeyt.com`,
+    siteDescription: `We build AI agents and set up automations for your business' success`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@mayknxyz`,
   },
   trailingSlash: `always`,
   plugins: [
@@ -60,6 +60,6 @@ const config: GatsbyConfig = {
       },
     },
   ].filter(Boolean) as Array<PluginRef>,
-}
+};
 
-export default config
+export default config;
